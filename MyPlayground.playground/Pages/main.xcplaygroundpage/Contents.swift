9978ff2ac05baeedc09bb0f154180afde002d389
EMG114 <egutierrez411@gmail.com>
2016-07-05 15:10:08 -0400

    my func

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -14,12 +14,21 @@
 // write your code here
 
 
+func helloWorld() {
+    print( "Hello, world!")
+}
 
+helloWorld()
 
 /*: question2
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+
+func myConst (){
+let myConst1=67
+    print (myConst1)}
+myConst()
 
 
 
@@ -30,8 +39,12 @@
 // write your code here
 
 
+func greet (personName: String){
+    print ("Hello \(personName)" )
+}
 
-
+//greet("Maria")
+greet("John")
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
@@ -39,12 +52,21 @@
 
 
 
-
+let myFriend = "Jose"
+greet(myFriend)
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
 
+
+func myWay (){
+    
+   // var name:String = "Erica"
+ 
+
+
+}
 
 
 
